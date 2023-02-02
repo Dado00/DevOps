1b10ba455badf247aed53ae8dd395756252fbe2c
Dado00 <62186281+Dado00@users.noreply.github.com>
2023-02-02 16:22:57 -0600

    Update pokemon.sh

--- pokemon.sh
+++ pokemon.sh
@@ -5,11 +5,11 @@ existe=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada);
 if [[ "$existe" == "Not Found" ]]; then 
 	echo "No se ha encontrado el pokemon";	
 else
-	clave=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .id);
-	numero=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .order);
-	nombre=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .name);
-	tamano=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .height);
-	peso=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .weight);
-	echo "ID={$clave}, name={$nombre}, weight={$peso}, height={$tamano}, order={$numero}";
+	indice=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .id);
+	order=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .order);
+	name=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .name);
+	height=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .height);
+	weight=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .weight);
+	echo "ID={$id}, name={$name}, weight={$weight}, height={$height}, order={$order}";
 
 fi
