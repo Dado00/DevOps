377d6580652d178c6f767e99abb7ca6cc2550adc
Dado00 <62186281+Dado00@users.noreply.github.com>
2023-02-02 15:51:29 -0600

    Create pokemon.sh

---
+++ pokemon.sh
@@ -0,0 +1,27 @@
+pokemonEntrada=$1;
+
+validacion=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada); 
+
+if [[ "$validacion" == "Not Found" || "$validacion" == "null" ]]; then 
+
+	clear;
+
+	echo "Ese pokemon no existe";
+	
+else
+
+	indice=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .id);
+
+	name=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .name);
+
+	weight=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .weight);
+
+	height=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .height);
+
+	order=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonEntrada | jq .order);
+	
+	clear;
+	
+	echo "ID={$indice}, name={$name}, weight={$weight}, height={$height}, order={$order}";
+
+fi
